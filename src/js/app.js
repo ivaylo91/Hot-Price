833436825872248fbc99ee1b6a67f78f246e09a1
ivaylo91 <ipenev91@gmail.com>
2022-12-15 18:39:03 +0200

    refactor code

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,8 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
     // This block will be executed once the page is loaded and ready
-    let elements = document.querySelector(".price");
+    let elements = document.querySelectorAll(".price");
+    console.log(elements);
     elements.forEach(function (el) {
         if (el.classList.contains("hot")) {
             el.innerHTML += "&#128293";
